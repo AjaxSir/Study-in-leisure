87d9ce42ca900b82f30c1b119052ec8882649555
xiaolong.su <xiaolong.su@bstai.top>
2024-08-20 14:01:33 +0800

    add: 数组的参数归一化 :bug:

---
+++ js/数组分组思考.js
@@ -0,0 +1,49 @@
+﻿var arr = [
+    {
+        name: 'Tom',
+        age: 18,
+        gender: 'Male',
+
+    },
+    {
+        name: 'Jerry',
+        age: 25,
+        gender: 'Male',
+    },
+    {
+        name: 'Lily',
+        age: 16,
+        gender: 'Female',
+    },
+    {
+        name: 'Bob',
+        age: 22,
+        gender: 'Male',
+    },
+    {
+        name: 'Emily',
+        age: 20,
+        gender: 'Female',
+    }
+]
+
+const groupBy = (arr, callback) => {
+    const result = {}
+    let generateKey = null
+    if (typeof callback == 'string') {
+        generateKey = (item) => item[callback]
+    } else {
+        generateKey = callback
+    }
+    for(let item of arr) {
+        const prop = generateKey(item)
+        if (!result[prop]) {
+            result[prop] = [item]
+        } else {
+            result[prop].push(item)
+        }
+    }
+    return result;
+}
+// console.log(groupBy(arr, (item) => `${item.age}-${item.gender}`))
+console.log(groupBy(arr, 'age'))
