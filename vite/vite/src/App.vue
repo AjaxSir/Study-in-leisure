011f64f6ebf9ff8ac1e4e2dedd35089e1e874a52
xiaolong.su <xiaolong.su@bstai.top>
2024-09-02 14:22:00 +0800

    add: 使用defer 首屏优化

--- vite/vite/src/App.vue
+++ vite/vite/src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2024-05-23 14:25:08
  * @LastEditors: xiaolong.su@bst.ai
- * @LastEditTime: 2024-08-28 09:49:50
+ * @LastEditTime: 2024-09-02 14:08:52
  * @Description: 
 -->
 <script setup lang="ts">
@@ -9,7 +9,8 @@
 // import Responsive from './components/Responsive.vue'
 // import ClipPath from './components/clipPath.vue';
 // import AnimateSvg from './components/animateSvg.vue';
-import ProgressiveImg from './components/ProgressiveImage.vue';
+ // import ProgressiveImg from './components/ProgressiveImage.vue';
+ import Defer from './components/defer.vue'
 import { ref } from 'vue'
 import  useStore from './store/pinia'
 const { counterStore } = useStore()
@@ -24,7 +25,8 @@ const value = ref(0)
   <Responsive :value="value" /> -->
   <!-- <ClipPath /> -->
   <!-- <AnimateSvg /> -->
-  <ProgressiveImg />
+  <!-- <ProgressiveImg /> -->
+  <Defer />
   <!-- vuex -->
     <!-- <span>{{ store.state.counter.counter }}</span> -->
   <!-- <button @click="value+= 1">incres</button> -->
@@ -32,8 +34,8 @@ const value = ref(0)
   <!-- <button @click="store.dispatch('addCounterAction', 1)">incres counter</button> -->
 
     <!-- pinia -->
-    <span>{{ counterStore.count }}</span>
-    <button @click="counterStore.increment()">pinia add</button>
+    <!-- <span>{{ counterStore.count }}</span>
+    <button @click="counterStore.increment()">pinia add</button> -->
 </template>
 
 <style scoped>
---
+++ vite/vite/src/components/defer.vue
@@ -0,0 +1,150 @@
+﻿<!--
+ * @Date: 2024-09-02 13:53:50
+ * @LastEditors: xiaolong.su@bst.ai
+ * @LastEditTime: 2024-09-02 14:20:18
+ * @Description: 
+-->
+<template>
+    <div>111</div>
+    <div class="d" v-for="idx in 10000">
+        <!-- <ProgressiveImage /> -->
+        <div  v-if="defer(idx)">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import ProgressiveImage from './ProgressiveImage.vue'
+    const count = ref(0)
+    const useDefer = (max = 10000) => {
+        const fns = () => requestAnimationFrame(() => {
+            count.value = count.value + 1;
+            if (count.value < max) fns()
+        })
+        fns()
+        return (counts) => {
+            return count.value >= counts
+        }
+    }
+    const defer = useDefer()
+</script>
+
+<style  scoped>
+.d{
+    width: 200px;
+    height: 200px;
+    float: left;
+    border: 1px solid black;
+}
+.d>div {
+    display: grid;
+    height: 100%;
+    width: 100%;
+    grid-template-rows: repeat(10, 1fr);
+    grid-template-columns: repeat(10, 1fr);
+}
+.d>div>div{
+    border: 1px solid red;
+}
+</style>
